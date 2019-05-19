254706ebcd1450db58516faf84387c94818016cd
Mykhailo <budda_2@mail.ru>
2019-05-19 20:00:19 +0300

    Addition Generics examples

---
+++ Lectures/Lection1/Generic/Generic/Generics/Printer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Generic.Generics
+{
+    //non-generic class
+    public class Printer : IPrinter
+    {
+        //we do not care
+        public string Name => throw new NotImplementedException();
+
+        //we do not care
+        public Uri Uri => throw new NotImplementedException();
+
+        //Implementation of generic method from non-generic interface
+        public void Print<T>(T itemToPrint)
+        {
+            Console.WriteLine(itemToPrint.ToString());
+        }
+    }
+}
