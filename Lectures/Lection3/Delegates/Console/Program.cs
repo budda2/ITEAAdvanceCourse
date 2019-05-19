323a521c546cb4e9e66ec0ff241bbbde32bcdcc7
Mykhailo <budda_2@mail.ru>
2019-05-19 21:25:54 +0300

    delegates

---
+++ Lectures/Lection3/Delegates/Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using Delegates;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Student("Mark");
+            var otherStudent = new Student("John");
+            var lecturer = new Lecturer("Isaak");
+            var handleStudents = new HandleStudentDelegate(lecturer.RateStudent);
+            HandleStudentDelegate lambdaHandleStudent = (Student s) => System.Console.WriteLine(s.Name);
+            HandleStudentDelegate anonymusMethodHandleStudent = delegate (Student s)
+            {
+                System.Console.WriteLine(s.Name);
+            };
+            handleStudents(student);
+        }
+    }
+}
