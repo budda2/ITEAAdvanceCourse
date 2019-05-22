c249cfe061b4ab14ca76d0a4151ce1eb5bb5230a
Mykhailo <budda_2@mail.ru>
2019-05-22 22:56:25 +0300

    Commit

---
+++ Lectures/Lection3/Delegates/Delegates/Counter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Delegates
+{
+    public class Counter
+    {
+        public event HandleHundredDelegate HundredReached;
+
+        public void Count()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i == 100)
+                {
+                    //to raise an event.
+                    HundredReached?.Invoke();
+                    RaiseHundredreached();
+                    i = 0;
+                }
+            }
+        }
+
+        // Explicitly calls all the handler of event.
+        //In case of exception on handler execution, other handlers will be executed
+        private void RaiseHundredreached()
+        {
+            if (HundredReached == null)
+                return;
+
+            var exceptions = new List<Exception>();
+            foreach(HandleHundredDelegate handler in HundredReached.GetInvocationList())
+            {
+                try
+                {
+                    handler.Invoke();
+                }
+                catch(Exception e)
+                {
+                    exceptions.Add(e);
+                }
+            }
+            if (exceptions.Count > 0)
+                throw new AggregateException(exceptions);            
+        }
+    }
+}
